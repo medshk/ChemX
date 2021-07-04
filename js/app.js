ac8842fb5c42a9bd1214669c53f69ac7901da74b
youcefkh <youcefkh13.yk@gmail.com>
2021-07-04 21:53:26 +0000

    clean code / add few classes

--- js/app.js
+++ js/app.js
@@ -8,8 +8,9 @@ class Polygone{
         this.rotation = rotation;
         this.atoms = [];
         this.bonds = [];
+        this.lines = []; //array of lines that constitute the polygone (Sides)
         this.element = null;
-        this.draw();
+        this.draw(); //draw the polygone
     }
 
     draw(){
@@ -76,10 +77,13 @@ class Polygone{
     drawPolygByLine(circles){
         circles.forEach((c, index) => {
             if (index == circles.length -1){
-                Polygone.drawLine(c.left, c.top, circles[0].left, circles[0].top, c.originCenter)
+                const line = Canvas.drawLine(c.left, c.top, circles[0].left, circles[0].top, c.originCenter);
+                this.lines.push(line)
             }
-            else
-            Polygone.drawLine(c.left, c.top, circles[index+1].left, circles[index+1].top, c.originCenter)
+            else{
+                const line = Canvas.drawLine(c.left, c.top, circles[index+1].left, circles[index+1].top, c.originCenter);
+                this.lines.push(line)
+            }
         });
     }
    
@@ -122,38 +126,140 @@ class Polygone{
 
     return circles;
     }
-    
-    static getPointFromOrigin (origin, point, distance){
-        const x = point.x
-        const y = point.y
-        const originX = origin.x
-        const originY = origin.y
-        
-        const deltaX = x - originX
-        const deltaY = y - originY
-      
-        let angle = Math.atan2(deltaY, deltaX)// radian
-        
-        const x2 = x + distance * Math.cos(angle)
-        const y2 = y + distance * Math.sin(angle)
-        
-        angle = ( angle * 180 / Math.PI ) + 90; //transform to degree; +90 to adapt to our origin
-        
-        return {x: x2, y: y2, angle: angle}
-    }
 
-    static drawLine(x1, y1, x2, y2, origin = false){
-        const line = new fabric.Line([x1, y1, x2, y2], {
-            type: "line",
+    static draw2(circle, nbrSides, radius){
+        let angle = 0;
+        let originRadius = 0;
+        switch (nbrSides) {
+            case 3:
+                radius = radius/1.5;
+                originRadius = radius - 8;
+                angle = 90;
+                break;
+            case 5:
+                originRadius = radius -5;
+                angle = 180;
+                break;
+        
+            default:
+                break;
+        }
+        const bondOrigin = circle.originCenter
+        const x2 = circle.left
+        const y2 = circle.top
+  
+        const polygOrigin = Canvas.getPointFromOrigin(bondOrigin, {x: x2, y: y2}, originRadius)//5: rad-5, 3:rad-7
+  
+        return new Polygone(polygOrigin.x, polygOrigin.y, nbrSides, radius, polygOrigin.angle + angle) //5:angle+180 / 3:angle+90
+    }
+}
+
+//Benzene class: hexagone(6) with bonds inside
+class Benzene extends Polygone{
+    constructor(x, y, radius, rotation = 0){
+        super(x, y, 6, radius, rotation);
+        this.rotation = rotation;
+    }
+    draw(){
+        const points = this.regularPolygonPoints(this.nbrSides, this.radius)
+
+        this.rotation += 30;
+
+        this.element = new fabric.Polygon(points, {
+            //id: polygonCounter++,
+            type: "polygone",
+            top: this.y,
+            left: this.x,
+            fill: '',
             stroke: 'black',
-            strokeWidth: 2,
-            originCenter: {x: origin.x, y: origin.y},
+            strokeWidth: 3,
+            originX: "center",
+            originY: "center",
+            angle: this.rotation,
             hasControls: false,
             hasBorders: false,
             selectable: false
         });
-    
-        canvas.add(line).requestRenderAll()
+
+        //get corners coord
+        let polygPoints = this.addCircles(this.element)
+        
+        //get the centroid
+        let centroid = this.getPolygCentroid(polygPoints)
+        
+        //add centroid coord to polygone attributes
+        this.element['center'] = centroid
+
+        //add circles
+        let circles = this.addCircles(this.element)
+
+        //draw polygone using lines
+        this.drawPolygByLine(circles)
+
+        //add bonds inside the polygone
+        this.lines.forEach((line, index) => {
+            if( [0, 2, 4].includes(index) ){
+                SimpleBond.drawInsidePolyg(line)
+            }
+        })
+
+
+        canvas.add(...circles).requestRenderAll();
+    }
+}
+
+//Cyclopentadiene class: pentagone(5) with bonds inside
+class Cyclopentadiene extends Polygone{
+    constructor(x, y, radius, rotation = 0){
+        super(x, y, 5, radius, rotation);
+        this.rotation = rotation;
+    }
+
+    draw(){
+        const points = this.regularPolygonPoints(this.nbrSides, this.radius)
+
+        this.rotation += 270;
+
+        this.element = new fabric.Polygon(points, {
+            //id: polygonCounter++,
+            type: "polygone",
+            top: this.y,
+            left: this.x,
+            fill: '',
+            stroke: 'black',
+            strokeWidth: 3,
+            originX: "center",
+            originY: "center",
+            angle: this.rotation,
+            hasControls: false,
+            hasBorders: false,
+            selectable: false
+        });
+
+        //get corners coord
+        let polygPoints = this.addCircles(this.element)
+        
+        //get the centroid
+        let centroid = this.getPolygCentroid(polygPoints)
+        
+        //add centroid coord to polygone attributes
+        this.element['center'] = centroid
+
+        //add circles
+        let circles = this.addCircles(this.element)
+
+        //draw polygone using lines
+        this.drawPolygByLine(circles)
+
+        //add bonds inside the polygone
+        this.lines.forEach((line, index) => {
+            if( [1, 3].includes(index) ){
+                SimpleBond.drawInsidePolyg(line)
+            }
+        })
+
+
+        canvas.add(...circles).requestRenderAll();
     }
 }
 
@@ -205,7 +311,7 @@ class SimpleBond{
     }
 
     draw(){
-        Polygone.drawLine(this.point1.x, this.point1.y, this.point2.x, this.point2.y)
+        Canvas.drawLine(this.point1.x, this.point1.y, this.point2.x, this.point2.y)
         if (this.isAddCircle){
             this.addCircle()
         }
@@ -228,8 +334,69 @@ class SimpleBond{
     
         canvas.add(circle).requestRenderAll()
     }
+
+    static drawInsidePolyg (line){
+        const p1 = {x: line.x1, y: line.y1}
+        const p2 = {x: line.x2, y: line.y2}
+        //coord of the new point 1
+        const x1 = Canvas.getPointFromOrigin(line.originCenter, {x: p1.x, y: p1.y}, -10).x 
+        const y1 = Canvas.getPointFromOrigin(line.originCenter, {x: p1.x, y: p1.y}, -10).y
+        //coord of the new point 2
+        const x2 = Canvas.getPointFromOrigin(line.originCenter, {x: p2.x, y: p2.y}, -10).x 
+        const y2 = Canvas.getPointFromOrigin(line.originCenter, {x: p2.x, y: p2.y}, -10).y
+  
+        return new SimpleBond({x: x1, y: y1}, {x: x2, y: y2}, false)
+    }
+
+    static drawOutsidePolyg (circle){
+        const x = circle.left
+        const y = circle.top
+        const point = Canvas.getPointFromOrigin(circle.originCenter, {x, y}, 50)
+        const x2 = point.x
+        const y2 = point.y
+        //const lineAngle = point.angle
+  
+        //add a simple bond
+        return new SimpleBond({x: x, y: y}, {x: x2, y: y2})
+    }
 }
 
+//Canvas class: handle drawing
+class Canvas{
+    static getPointFromOrigin (origin, point, distance){
+        const x = point.x
+        const y = point.y
+        const originX = origin.x
+        const originY = origin.y
+        
+        const deltaX = x - originX
+        const deltaY = y - originY
+      
+        let angle = Math.atan2(deltaY, deltaX)// radian
+        
+        const x2 = x + distance * Math.cos(angle)
+        const y2 = y + distance * Math.sin(angle)
+        
+        angle = ( angle * 180 / Math.PI ) + 90; //transform to degree; +90 to adapt to our origin
+        
+        return {x: x2, y: y2, angle: angle}
+    }
+
+    static drawLine(x1, y1, x2, y2, origin = false){
+        const line = new fabric.Line([x1, y1, x2, y2], {
+            type: "line",
+            stroke: 'black',
+            strokeWidth: 2,
+            originCenter: {x: origin.x, y: origin.y},
+            hasControls: false,
+            hasBorders: false,
+            selectable: false
+        });
+    
+        canvas.add(line).requestRenderAll()
+        return line;
+    }
+}
 
 //set the fabric canvas
 const canvas = new fabric.Canvas('canvas');
@@ -244,41 +411,20 @@ canvas.on('mouse:down', function(e){
     const y = e.pointer.y
 
     if (e.target && e.target.type == "circle"){
-        //canvas.renderAll();
-        const x = e.target.left
-        const y = e.target.top
-        const point = Polygone.getPointFromOrigin(e.target.originCenter, {x, y}, 50)
-        const x2 = point.x
-        const y2 = point.y
-        const lineAngle = point.angle
-  
-        //add a simple bond
-        return new SimpleBond({x: x, y: y}, {x: x2, y: y2})
+
+        return SimpleBond.drawOutsidePolyg(e.target)
   
     }else if(e.target && e.target.type == "line"){
 
-        const p1 = {x: e.target.x1, y: e.target.y1}
-        const p2 = {x: e.target.x2, y: e.target.y2}
-        //coord of the new point 1
-        const x1 = Polygone.getPointFromOrigin(e.target.originCenter, {x: p1.x, y: p1.y}, -10).x 
-        const y1 = Polygone.getPointFromOrigin(e.target.originCenter, {x: p1.x, y: p1.y}, -10).y
-        //coord of the new point 2
-        const x2 = Polygone.getPointFromOrigin(e.target.originCenter, {x: p2.x, y: p2.y}, -10).x 
-        const y2 = Polygone.getPointFromOrigin(e.target.originCenter, {x: p2.x, y: p2.y}, -10).y
-  
-        return new SimpleBond({x: x1, y: y1}, {x: x2, y: y2}, false)
-
+        return SimpleBond.drawInsidePolyg(e.target)
+        
     }else if(e.target && e.target.type == "bond"){
-        const bondOrigin = e.target.originCenter
-        const x2 = e.target.left
-        const y2 = e.target.top
-  
-        const polygOrigin = Polygone.getPointFromOrigin(bondOrigin, {x: x2, y: y2}, polygRadius-5)//5: rad-5, 3:rad-7
-  
-        return new Polygone(polygOrigin.x, polygOrigin.y, 5, polygRadius, polygOrigin.angle +180) //5:angle+180 / 3:angle+90
+
+        return Polygone.draw2(e.target, 3, polygRadius)
   
       }else{
-        return new Polygone(x, y, 6, polygRadius)
+        return new Cyclopentadiene(x, y, polygRadius)
+        //return new Polygone(x, y, 5, polygRadius) //5:angle+180 / 3:angle+90
     }
 
 });
@@ -296,5 +442,4 @@ canvas.on('mouse:out', function(e){
         const circle = e.target;
         Atom.setBorderColorToDefault(circle)
     }
-    //Atom.setBorderColorToDefault()
 })
